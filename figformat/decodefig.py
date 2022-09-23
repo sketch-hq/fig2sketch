3c67804fc1dd3fef49aa577274fb4e6e69aa1a22
Esther Lozano <esloho@gmail.com>
2022-09-23 18:55:55 +0200

    😈 Support new Figma export format

--- figformat/decodefig.py
+++ figformat/decodefig.py
@@ -1,11 +1,16 @@
 import io
 import zlib
+import zipfile
 import struct
 from .kiwi import *
 
 
 def decode(reader):
     figma = reader.read()
+
+    if figma.startswith(b'PK'):
+        figma_zip = zipfile.ZipFile(io.BytesIO(figma))
+        figma = figma_zip.open('canvas.fig').read()
 
     offset = 12
     segments = []
