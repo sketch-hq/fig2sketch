0df062a8750542a278d1091714e8e74487d2938c
Javier Torres <javier@javiertorres.eu>
2022-10-03 16:16:21 +0200

    ₥ Simplify matches in component.py

--- converter/component.py
+++ converter/component.py
@@ -4,24 +4,15 @@ import utils
 def convert(figma_style):
     match figma_style:
         # Fill with a single fill
-        case {'styleType': 'FILL', 'fillPaints': [paint]}:
-
-            return convert_fill(figma_style, paint)
-        case _:
-            raise Exception(f"Unsupported shared style: '{figma_style['styleType']}'")
-
-
-def convert_fill(figma_style, figma_fill):
-    match figma_fill:
-        case {'type': 'SOLID'}:
+        case {'styleType': 'FILL', 'fillPaints': [{'type': 'SOLID'} as paint]}:
             return {
                 '_class': 'swatch',
                 'do_objectID': utils.gen_object_id(figma_style.id),
                 'name': figma_style.name,
                 'value': {
                     '_class': 'color',
-                    **style.convert_color(figma_style['fillPaints'][0])
+                    **style.convert_color(paint)
                 }
             }
         case _:
-            return None
+            raise Exception(f"Unsupported shared style: '{figma_style['styleType']}'")
