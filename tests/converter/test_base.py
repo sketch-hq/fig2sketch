855cb04e24199c172ec88719e7a6b2a3421b05a2
Javier Torres <javier@javiertorres.eu>
2022-11-28 22:40:41 +0100

    🥁 Some base tests about style overrides

---
+++ tests/converter/test_base.py
@@ -0,0 +1,143 @@
+from .base import *
+from converter import prototype, tree, base
+from converter.positioning import Matrix
+from sketchformat.layer_common import ClippingMaskMode
+from sketchformat.style import *
+import pytest
+from unittest.mock import ANY
+from converter.context import context
+
+FIG_ARTBOARD = {
+    **FIG_BASE,
+    "type": "FRAME",
+    "resizeToFit": False,
+    "children": [],
+}
+
+
+@pytest.fixture
+def no_prototyping(monkeypatch):
+    monkeypatch.setattr(prototype, "prototyping_information", lambda _: {})
+
+
+@pytest.mark.usefixtures("no_prototyping")
+class TestArtboardBackgroud:
+    def test_no_background(self):
+        ab = tree.convert_node(FIG_ARTBOARD, "CANVAS")
+
+        assert ab.hasBackgroundColor == False
+
+    def test_disabled_background(self):
+        ab = tree.convert_node(
+            {
+                **FIG_ARTBOARD,
+                "fillPaints": [
+                    {
+                        "type": "SOLID",
+                        "color": FIG_COLOR[0],
+                        "opacity": 0.9,
+                        "visible": False,
+                    }
+                ],
+            },
+            "CANVAS",
+        )
+
+        assert ab.hasBackgroundColor == False
+
+    def test_simple_background(self):
+        ab = tree.convert_node(
+            {
+                **FIG_ARTBOARD,
+                "fillPaints": [
+                    {
+                        "type": "SOLID",
+                        "color": FIG_COLOR[0],
+                        "opacity": 0.9,
+                        "visible": True,
+                    }
+                ],
+            },
+            "CANVAS",
+        )
+
+        assert ab.hasBackgroundColor == True
+        assert ab.backgroundColor == SKETCH_COLOR[0]
+
+    def test_gradient_background(self, warnings):
+        ab = tree.convert_node(
+            {
+                **FIG_ARTBOARD,
+                "fillPaints": [
+                    {
+                        "type": "GRADIENT_LINEAR",
+                        "transform": Matrix([[0.7071, -0.7071, 0.6], [0.7071, 0.7071, -0.1]]),
+                        "stops": [
+                            {"color": FIG_COLOR[0], "position": 0},
+                            {"color": FIG_COLOR[1], "position": 0.4},
+                            {"color": FIG_COLOR[2], "position": 1},
+                        ],
+                        "visible": True,
+                    }
+                ],
+            },
+            "CANVAS",
+        )
+
+        assert ab.hasBackgroundColor == False
+        assert len(ab.layers) == 1
+        bg = ab.layers[0]
+        assert len(bg.style.fills) == 1
+        assert bg.style.fills[0].fillType == FillType.GRADIENT
+
+        warnings.assert_any_call("ART003", ANY)
+
+
+FIG_TEXT = {
+    **FIG_BASE,
+    "type": "TEXT",
+    "fontName": {"family": "Roboto", "style": "Normal"},
+    "fontSize": 12,
+    "textAlignVertical": "CENTER",
+    "textAlignHorizontal": "CENTER",
+    "fillPaints": [{"type": "SOLID", "color": FIG_COLOR[0], "opacity": 1, "visible": True}],
+    "strokeAlign": "CENTER",
+    "strokeWeight": 1,
+}
+
+FIG_COLOR_STYLE = {
+    **FIG_BASE,
+    "type": "ROUNDED_RECTANGLE",
+    "fillPaints": [{"type": "SOLID", "color": FIG_COLOR[1], "opacity": 0.7, "visible": True}],
+}
+
+FIG_TEXT_STYLE = {
+    **FIG_BASE,
+    "type": "TEXT",
+    "fontName": {"family": "CustomFont", "style": "Normal"},
+}
+
+
+@pytest.fixture
+def style_overrides(monkeypatch):
+    context.init(None, {(0, 1): FIG_TEXT_STYLE, (0, 2): FIG_COLOR_STYLE})
+
+
+@pytest.mark.usefixtures("style_overrides")
+class TestInheritStyle:
+    def test_apply_fill_override(self):
+        style = base.process_styles({**FIG_TEXT, "inheritFillStyleID": (0, 2)})
+        assert style.fills[0].color == SKETCH_COLOR[1]
+
+    def test_apply_border_override(self):
+        style = base.process_styles({**FIG_TEXT, "inheritFillStyleIDForStroke": (0, 2)})
+        assert style.borders[0].color == SKETCH_COLOR[1]
+
+    def test_apply_text_override(self):
+        fig = {**FIG_TEXT, "inheritTextStyleID": (0, 1)}
+
+        # Fig will be modified in place with text styles, to be processed by text
+        base.process_styles(fig)
+
+        assert fig["fontName"] == FIG_TEXT_STYLE["fontName"]
+        assert fig["fontSize"] == 12
